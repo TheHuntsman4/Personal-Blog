2d6ea38811d47f4429068a997929be0a0b99f310
TheHuntsman4 <anikethvij464@gmail.com>
2023-12-13 02:16:30 +0530

    <feat>: [adds reversing of posts order from latest to oldest]

--- personal_blog/src/services/index.js
+++ personal_blog/src/services/index.js
@@ -1,46 +1,48 @@
-import { request, gql } from 'graphql-request';
-
+import { request, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export const getPosts = async () => {
   const query = gql`
-  query MyQuery {
-    postsConnection {
-      edges {
-        node {
-          author {
-            bio
-            name
-            id
-            photo {
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
               url
             }
-          }
-          createdAt
-          slug
-          title
-          excerpt
-          featuredImage {
-            url
-          }
-          categories {
-            category
-            slug
+            categories {
+              category
+              slug
+            }
           }
         }
       }
     }
-  }
   `;
 
   const result = await request(graphqlAPI, query);
+  console.log(result);
+  const posts= result.postsConnection.edges;
+  const revPosts=posts.reverse();
+  return revPosts;
 
-  return result.postsConnection.edges;
 };
 
-export const getRecentPosts= async()=>{
-  const query= gql`
+export const getRecentPosts = async () => {
+  const query = gql`
   query getpostdetails(){
     posts(
       orderBy: createdAt_ASC
@@ -53,16 +55,19 @@ export const getRecentPosts= async()=>{
       createdAt
       slug
     }
-  }`
-const result=await request(graphqlAPI,query);
-return result.posts;
-}
+  }`;
+  const result = await request(graphqlAPI, query);
+  return result.posts;
+};
 
 export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
-        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        where: {
+          slug_not: $slug
+          AND: { categories_some: { slug_in: $categories } }
+        }
         last: 3
       ) {
         title
@@ -79,31 +84,32 @@ export const getSimilarPosts = async (categories, slug) => {
   return result.posts;
 };
 
-export const getCategories=async()=>{
-  const query=gql`
-    query GetCategories{
-      categories{
+export const getCategories = async () => {
+  const query = gql`
+    query GetCategories {
+      categories {
         category
         slug
-        categoryImage{
+        categoryImage {
           url
         }
       }
-    }`
-    const result=await request(graphqlAPI,query);
-    return result.categories;
-}
+    }
+  `;
+  const result = await request(graphqlAPI, query);
+  return result.categories;
+};
 
 export const getPostDetails = async (slug) => {
   const query = gql`
-    query GetPostDetails($slug : String!) {
-      post(where: {slug: $slug}) {
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
         title
         excerpt
         featuredImage {
           url
         }
-        author{
+        author {
           name
           bio
           photo {
@@ -126,12 +132,12 @@ export const getPostDetails = async (slug) => {
   const result = await request(graphqlAPI, query, { slug });
 
   return result.post;
-}
+};
 
 export const getCategoryPost = async (slug) => {
   const query = gql`
     query GetCategoryPost($slug: String!) {
-      postsConnection(where: {categories_some: {slug: $slug}}) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
         edges {
           cursor
           node {
