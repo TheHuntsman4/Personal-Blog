39cf4d2db69b57606b359cd22b7618414614028c
TheHuntsman4 <anikethvij464@gmail.com>
2023-12-13 19:07:48 +0530

    <feat>: [removes font from post card]

--- personal_blog/src/components/PostCard.jsx
+++ personal_blog/src/components/PostCard.jsx
@@ -6,7 +6,7 @@ const PostCard = ({post}) => {
   console.log(post)
   
   return (
-    <div className='bg-white bg-opacity-[75%] flex flex-col justify-center items-center font-Special shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+    <div className='bg-white bg-opacity-[75%] flex flex-col justify-center items-center font-semibold shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
       <div className='relative z-10 overflow-hidden shadow-md '>
         <img
         src={post.featuredImage.url}
@@ -14,17 +14,17 @@ const PostCard = ({post}) => {
         className='object-top h-full w-full object-cover rounded-lg shadow-lg rounded-t-lg:rounded-lg'
         />
       </div>
-      <h1 className=" text-center text-[1.75rem] md:text-[3rem] cursor-pointer text-black font-Roboto py-[1rem]">
+      <h1 className=" text-center text-[1.75rem] md:text-[3rem] cursor-pointer text-black   py-[1rem]">
         <Link href={`/post/${post.slug}`}>{post.title}</Link>
       </h1>
       <div className='block lg:flex text-center items-center justify-center mb-8 w-full'>
       </div>
-      <div className='px-[2rem] text-black font-Roboto'>
-      <p className='align-center text-black font-Roboto text-[1.2rem] pl-[2rem] lg:px-20 '>{post.excerpt}</p>
+      <div className='px-[2rem] text-black  '>
+      <p className='align-center text-black   text-[1.2rem] pl-[2rem] lg:px-20 '>{post.excerpt}</p>
       </div>
       <div className="text-right mb-3 mr-4">
       <Link href={`/post/${post.slug}`}>
-        <span className='transition duration-500 font-Roboto font-semibold text-white  text-center inline-block bg-black rounded-full cursor-pointer hover:text-black hover:bg-white px-8 py-3 my-[2rem]'>Continue Reading
+        <span className='transition duration-500   font-semibold text-white  text-center inline-block bg-black rounded-full cursor-pointer hover:text-black hover:bg-white px-8 py-3 my-[2rem]'>Continue Reading
         </span></Link>
        </div> 
     </div>
